2a61c42bdaa1f92bfaaaded8a22e53cd6fc5f910
vmuruganandam <2011vmurugan@gmail.com>
2013-12-06 17:52:38 -0500

    singleton

--- twitter_db_ops.py
+++ twitter_db_ops.py
@@ -4,16 +4,15 @@ import os
 import re
 
 class Singleton:
-  def __init__(self, klass):
-    self.klass = klass
-    self.instance = None
-  def __call__(self, *args, **kwds):
-    if self.instance == None:
-      self.instance = self.klass(*args, **kwds)
-    return self.instance
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
 
 @Singleton
 class TwitterDbOps:
+    __metaclass__ = Singleton
     def __init__(self, path):
         if not path:
             self.path = ".twitteranalyzer.db"
